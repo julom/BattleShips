6e966c690c69b7ae774e7cb1f03c2a000c4dcde4
Juliusz Mały <julo3387@gmail.com>
2019-07-16 02:41:38 +0200

    Added project Core

---
+++ BattleShips.Core/GameSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BattleShips.Core
+{
+    public static class GameSettings
+    {
+        public const int BoardSizeX = 10;
+        public const int BoardSizeY = 10;
+    }
+}
