39430da997cfde35fd1115c6df3eff60d4582939
Juliusz Mały <julo3387@gmail.com>
2019-07-17 01:34:37 +0200

    added exceptions

---
+++ BattleShips.Core/Exceptions/GameArgumentException.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace BattleShips.Core.Exceptions
+{
+    public class GameArgumentException : ArgumentException
+    {
+    }
+}
---
+++ BattleShips.Core/Exceptions/GameLogicalException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BattleShips.Core.Exceptions
+{
+    class GameLogicalException : GameException
+    {
+    }
+}
