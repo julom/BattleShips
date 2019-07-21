ccd4211e0133f71b460ab209e1eda0428af7a6b7
Juliusz Mały <julo3387@gmail.com>
2019-07-22 01:21:37 +0200

    Changed GameSettings to be changeable

--- BattleShips.Core/GameSettings.cs
+++ BattleShips.Core/GameSettings.cs
@@ -4,8 +4,8 @@ namespace BattleShips.Core
 {
     public static class GameSettings
     {
-        public const int BoardSizeX = 10;
-        public const int BoardSizeY = 10;
+        public static int BoardSizeX = 10;
+        public static int BoardSizeY = 10;
 
         public static IList<int> ShipSizes = new List<int> {4, 5, 5};
     }
