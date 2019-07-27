04c654fda7404cecedb4fba7ae46d9447ed9f350
Juliusz Mały <julo3387@gmail.com>
2019-07-27 23:46:58 +0200

    added structs Coordinate and VectorLayout

---
+++ BattleShips.Core/GameEntities/Structs/Coordinate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BattleShips.Core.GameEntities.Structs
+{
+    public struct Coordinate
+    {
+        public int PositionX { get; }
+        public int PositionY { get; }
+
+        public Coordinate(int positionX, int positionY)
+        {
+            PositionX = positionX;
+            PositionY = positionY;
+        }
+    }
+}
---
+++ BattleShips.Core/GameEntities/Structs/VectorLayout.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace BattleShips.Core.GameEntities.Structs
+{
+    public struct VectorLayout
+    {
+        public ShipVector VectorX { get; }
+        public ShipVector VectorY { get; }
+        public IEnumerable<Coordinate> Values
+        {
+            get
+            {
+                foreach (var positionX in VectorX.Values)
+                {
+                    foreach (var positionY in VectorY.Values)
+                    {
+                        yield return new Coordinate(positionX, positionY);
+                    }
+                }
+            }
+        }
+
+        public VectorLayout(ShipVector vectorX, ShipVector vectorY)
+        {
+            VectorX = vectorX;
+            VectorY = vectorY;
+        }
+
+        public VectorLayout(int fromX, int toX, int fromY, int toY)
+        {
+            VectorX = new ShipVector(fromX, toX);
+            VectorY = new ShipVector(fromY, toY);
+        }
+    }
+}
