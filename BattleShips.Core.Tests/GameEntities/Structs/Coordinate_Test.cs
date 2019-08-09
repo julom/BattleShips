6adc64c20e9a55bbf0bf041ca96f2aaa0d7413ca
Juliusz Mały <julo3387@gmail.com>
2019-08-09 13:50:22 +0200

    added tests for Coordinate and ShipVector

---
+++ BattleShips.Core.Tests/GameEntities/Structs/Coordinate_Test.cs
@@ -0,0 +1,74 @@
+﻿using BattleShips.Core.Exceptions;
+using BattleShips.Core.GameEntities.Structs;
+using NUnit.Framework;
+using System;
+
+namespace BattleShips.Core.Tests.GameEntities.Structs
+{
+    [TestFixture]
+    public class Coordinate_Test
+    {
+        [Test, Pairwise]
+        public void GetNumericalPosition_HappyPath([Values("a", "A", "z", "Z")] string letter, [Range(1, 30)] int number)
+        {
+            var boardPosition = letter + number;
+
+            var result = Coordinate.GetNumericalPosition(boardPosition);
+
+            Assert.IsInstanceOf<Coordinate>(result);
+            Assert.IsNotNull(result);
+        }
+
+        [Test, Pairwise]
+        public void GetNumericalPosition_WrongNumber_ReturnsNull([Values("a", "A", "z", "Z")] string letter, [Values(-1,0)] int number)
+        {
+            var boardPosition = letter + number;
+
+            var result = Coordinate.GetNumericalPosition(boardPosition);
+
+            Assert.IsNull(result, "Coordinate should be null when number part is smaller than 1");
+        }
+
+        [Test, Pairwise]
+        public void GetNumericalPosition_WrongLetter_ReturnsNull([Values("1", ";", ",", "-", "@")] string letter, [Range(1, 10)] int number)
+        {
+            var boardPosition = letter + number;
+
+            var result = Coordinate.GetNumericalPosition(boardPosition);
+
+            Assert.IsNull(result, "Coordinate should be null when letter part is other sign then letter");
+        }
+
+        [Test, Pairwise]
+        public void GetNumericalPosition_InputNull_ReturnsNull()
+        {
+            string boardPosition = null;
+
+            var result = Coordinate.GetNumericalPosition(boardPosition);
+
+            Assert.IsNull(result, "Coordinate should be null when input is null");
+        }
+
+        [Test, Pairwise]
+        public void GetBoardPosition_HappyPath([Range(0, 20)] int positionX, [Range(0, 20)] int positionY)
+        {
+            var numericalPosition = new Coordinate(positionX, positionY);
+
+            var result = Coordinate.GetBoardPosition(numericalPosition);
+
+            Assert.IsInstanceOf<string>(result);
+            Assert.IsNotNull(result);
+            Assert.IsNotEmpty(result);
+        }
+
+        [Test, Sequential]
+        public void GetBoardPosition_EitherPositionIsNegative_([Values(-1, 0)] int positionX, [Values(0, -1)] int positionY)
+        {
+            var numericalPosition = new Coordinate(positionX, positionY);
+
+            Action action = () => Coordinate.GetBoardPosition(numericalPosition);
+
+            Assert.Throws<GameArgumentException>(() => action());
+        }
+    }
+}
---
+++ BattleShips.Core.Tests/GameEntities/Structs/ShipVector_Tests.cs
@@ -0,0 +1,58 @@
+﻿using BattleShips.Core.GameEntities.Structs;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BattleShips.Core.Tests.GameEntities.Structs
+{
+    [TestFixture]
+    public class ShipVector_Tests
+    {
+        [Test, Sequential]
+        public void ShipVector_HappyPath([Values(0, 1, 2, 3)] int from, [Values(0, 3, 5, 10)] int to)
+        {
+            var shipVector = new ShipVector(from, to);
+
+            Assert.NotNull(shipVector);
+            Assert.AreEqual(from, shipVector.From);
+            Assert.AreEqual(to, shipVector.To);
+        }
+
+        [Test, Sequential]
+        public void ShipVector_ValuesInReverseOrder_RevertsBack([Values(0, 3, 5, 10)] int from, [Values(0, 1, 2, 3)] int to)
+        {
+            var smallerValue = Math.Min(from, to);
+            var biggerValue = Math.Max(from, to);
+
+            var shipVector = new ShipVector(from, to);
+
+            Assert.AreEqual(smallerValue, shipVector.From);
+            Assert.AreEqual(biggerValue, shipVector.To);
+        }
+
+        [Test, Sequential]
+        public void Size_ReturnsCountOfValuesInside([Values(0, 1, 2, 3)] int from, [Values(0, 3, 5, 10)] int to)
+        {
+            var size = to - from + 1;
+
+            var shipVector = new ShipVector(from, to);
+
+            Assert.AreEqual(size, shipVector.Size);
+        }
+
+        [Test, Sequential]
+        public void Values_ReturnsValuesInside([Values(0, 1, 2, 3)] int from, [Values(0, 3, 5, 10)] int to)
+        {
+            var values = new List<int>();
+            for (int i = from; i <= to; i++)
+            {
+                values.Add(i);
+            }
+
+            var shipVector = new ShipVector(from, to);
+
+            Assert.AreEqual(values, shipVector.Values.ToList());
+        }
+    }
+}
