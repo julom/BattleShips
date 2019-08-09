66bef8201c0e221980ab89e4e4997427aec526a3
Juliusz Mały <julo3387@gmail.com>
2019-08-09 13:55:34 +0200

    added GameFactory

---
+++ BattleShips.Core/GameEntities/Factories/Abstract/IGameFactory.cs
@@ -0,0 +1,10 @@
+﻿using BattleShips.Core.GameEntities.Abstract;
+using BattleShips.Core.GameEntities.DifficultyLevels.Abstract;
+
+namespace BattleShips.Core.GameEntities.Factories.Abstract
+{
+    public interface IGameFactory
+    {
+        IGame Create(IShip[] playerShips, IDifficultyLevel difficulty);
+    }
+}
---
+++ BattleShips.Core/GameEntities/Factories/GameFactory.cs
@@ -0,0 +1,21 @@
+﻿using BattleShips.Core.GameEntities.Abstract;
+using BattleShips.Core.GameEntities.DifficultyLevels.Abstract;
+using BattleShips.Core.GameEntities.Factories.Abstract;
+
+namespace BattleShips.Core.GameEntities.Factories
+{
+    public class GameFactory : IGameFactory
+    {
+        private readonly IBoardFactory _boardFactory;
+
+        public GameFactory(IBoardFactory boardFactory)
+        {
+            _boardFactory = boardFactory;
+        }
+
+        public IGame Create(IShip[] playerShips, IDifficultyLevel difficulty)
+        {
+            return new Game(playerShips, _boardFactory, difficulty);
+        }
+    }
+}
